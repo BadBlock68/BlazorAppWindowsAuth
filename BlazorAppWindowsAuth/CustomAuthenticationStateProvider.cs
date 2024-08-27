b94f7db71c504a42994c2046c4d8445dbfd6c194
Peter Forsbom <peter@contesto.no>
2024-08-27 12:05:52 +0200

    Initial commit

---
+++ BlazorAppWindowsAuth/CustomAuthenticationStateProvider.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Components.Authorization;
+using System.Security.Claims;
+
+namespace BlazorAppWindowsAuth;
+
+public class CustomAuthenticationStateProvider : AuthenticationStateProvider
+{
+    private readonly IHttpContextAccessor _httpContextAccessor;
+
+    public CustomAuthenticationStateProvider(IHttpContextAccessor httpContextAccessor)
+    {
+        this._httpContextAccessor = httpContextAccessor;
+    }
+
+    public override async Task<AuthenticationState> GetAuthenticationStateAsync()
+    {
+        var identity = _httpContextAccessor.HttpContext.User.Identity;
+        var windowsAccountName = identity.Name.Split('\\').Last();
+
+        var ci = identity as ClaimsIdentity;
+        var user = new ClaimsPrincipal(identity);
+
+        user.AddIdentity(new ClaimsIdentity(new List<Claim>() { new Claim(ClaimTypes.Role, "Admin") }));
+        user.AddIdentity(new ClaimsIdentity(new List<Claim>() { new Claim(ClaimTypes.Role, "Manager") }));
+        return await Task.FromResult(new AuthenticationState(user));
+    }
+}
